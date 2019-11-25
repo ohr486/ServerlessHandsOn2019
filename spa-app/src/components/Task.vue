db97d4027e545d6ee859542f7ff3877f273024fd
ohr486 <ohr486@gmail.com>
2019-11-25 11:29:08 +0900

    add base task page

--- spa-app/src/components/Task.vue
+++ spa-app/src/components/Task.vue
@@ -1,9 +1,43 @@
 <template>
-  <p>Tasksページ</p>
+  <div id="task">
+    <table>
+      <tbody>
+        <tr>
+          <td>ID</td>
+          <td>Title</td>
+          <td>Desc</td>
+          <td>Status</td>
+        </tr>
+        <tr v-for="t in tasks" :key="t.id">
+          <td>{{ t.id }}</td>
+          <td>{{ t.title }}</td>
+          <td>{{ t.desc }}</td>
+          <td>{{ t.status }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  created () {
+    this.$axios
+      .get('http://localhost:3000/tasks.json', {headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}})
+      .then(response => {
+        this.tasks = response.data
+      })
+  }
+}
 </script>
 
 <style>
+.task {
+  color: red;
+}
 </style>
